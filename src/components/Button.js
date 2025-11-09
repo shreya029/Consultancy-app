4ac4e4dcf0fede1c3b507be1ddfd9e58ff674026
Dnyanashree Mutalik <dnyanashreemutalik@gmail.com>
2025-11-09 19:38:41 +0530

    commit all changes

--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button({children, onClick}){
-  return (
-    <button onClick={onClick} className="px-4 py-2 bg-blue-700 text-white rounded-md hover:bg-blue-800">
-      {children}
-    </button>
-  );
-}
